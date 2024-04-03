69db1ad848b4e5d6521f4992a043f5171d99b136
Khizar Pasha <khpa8186@colorado.edu>
2024-04-03 11:35:11 -0600

    Fixed syntax error in DB

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users(
     username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL
-    location VARCHAR(50) NOT NULL,
+    password CHAR(60) NOT NULL,
+    location VARCHAR(50) NOT NULL
 );
