6810b48651ff9b17e83dd37708f05b1db3d1b7ac
Austin McCutcheon <austinmcc1908@gmail.com>
2024-04-05 13:46:27 -0600

    Adding corrected create table and inert files, passed minimal testing reqs

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,5 +1,31 @@
-CREATE TABLE IF NOT EXISTS users(
-    username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL,
-    location VARCHAR(50) NOT NULL
+-- Drop tables if they exist
+DROP TABLE IF EXISTS user_artists;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS artists;
+
+-- Create users table
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    firstname VARCHAR(60),
+    lastname VARCHAR(60),
+    UNIQUE(username),
+    UNIQUE(email)
+);
+
+-- Create artists table
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id VARCHAR(255) PRIMARY KEY,
+    artist_name VARCHAR(120) NOT NULL,
+    UNIQUE(artist_id)
+);
+
+-- Create user_artists table
+CREATE TABLE IF NOT EXISTS user_artists (
+    follow_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    artist_id VARCHAR(255) REFERENCES artists(artist_id),
+    UNIQUE(user_id, artist_id)
 );
