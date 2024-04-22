f74eb40d5e4f0438044cd8e6d0b4fe5f6d40f251
Ethan Epperson <etep9497@colorado.edu>
2024-04-22 12:02:33 -0600

    got related artworks to work

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -245,6 +245,8 @@ app.get('/artwork/:id', async (req, res) => {
     const artworkData = await axios.get(api_url);
     const artwork = artworkData.data.data;
 
+    console.log(artwork);
+
     const img_src = `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`;
 
     const artworkInDb = await db.oneOrNone('SELECT * FROM artworks WHERE artwork_id = $1', [artwork.id]);
@@ -255,20 +257,12 @@ app.get('/artwork/:id', async (req, res) => {
       await db.none('INSERT INTO artworks VALUES ($1, $2, $3)', [artwork.id, artwork.title, img_src]);
     } // otherwise, proceed as normal
 
-    // elastic search style query to fetch artworks with same style 
-      // NOT WORKING ATM
-    const related_artworks_api_url = `https://api.artic.edu/api/v1/artworks/search?
-    {
-      "query": {
-      "terms": {
-        "style_id": ${artwork.style_id}, 
-        "is_public_domain": true
-      }
-    }&fields=id,title,image_id, description, artist_display&size=4`;
+    // searching for artworks with the same classification_id
+      // also test out same style id
+    const related_artworks_api_url = `https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&limit=4&fields=id,title,image_id, description, artist_display&q=${artwork.classification_id}`;
+  
     const related_artwork_data = await axios.get(related_artworks_api_url);
-    const related_artwork = related_artwork_data.data;
-
-    console.log(related_artwork);
+    const related_artwork = related_artwork_data.data.data;
 
     // console.log(artwork, related_artworks);
     res.render('pages/oneArtwork', {id: artwork.id, 
@@ -278,7 +272,7 @@ app.get('/artwork/:id', async (req, res) => {
       medium_display1: artwork.medium_display, 
       date_display1: artwork.date_display, 
       image_src1: img_src, 
-      related_artworks: related_artwork_data.data,
+      related_artworks: related_artwork,
       username: req.session.user.username, related_artwork});
   } catch(error) {
     console.log(error);
